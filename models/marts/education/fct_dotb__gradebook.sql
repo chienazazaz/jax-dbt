d4a179396e995f1a4154df1b330e479827e935af
Chien Le <lchien063@gmail.com>
2024-08-18 15:57:12 +0700

    fix: data type

--- models/marts/education/fct_dotb__gradebook.sql
+++ models/marts/education/fct_dotb__gradebook.sql
@@ -1,18 +1,24 @@
 {{
   config(
-    materialized = 'view',
+    materialized = 'table',
     )
 }}
 
+{# with gradebook as ( #}
 select 
 * except(detail_result, grade_config),
-json_value(detail_result,'$.value') as value,
-json_value(grade_config,'$.alias') as col,
-json_value(grade_config,'$.label') as label,
+safe_cast(json_value(detail_result,'$.value') as float64) as value,
+json_value(grade_config,'$.alias') as col_alias,
+regexp_replace(json_value(grade_config,'$.label'),r'\r\n',' ') as value_label,
 json_value(grade_config,'$.type') as value_type,
-json_value(grade_config,'$.max_mark') as max_mark,
+safe_cast(json_value(grade_config,'$.max_mark') as float64) as max_mark,
 json_value(grade_config,'$.formula') as formula,
+json_value(grade_config,'$.comment') as comment,
 from {{ ref("stg_dotb__gradebook") }}
 left join unnest(analytics.json_transform(parse_json( detail_result))) as detail_result
 left join unnest(analytics.json_extract_value(grade_config)) grade_config
 where json_value(grade_config,'$.alias') = json_value(detail_result,'$.key')
+and safe_cast(json_value(detail_result,'$.value') as float64) is not null
+{# ) #}
+
+
